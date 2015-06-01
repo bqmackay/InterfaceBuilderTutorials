c4e7e818f4516484d50f2ded0bf0bfc44089c8ea
Byron Mackay <byronm@mediarain.com>
2015-06-01 10:09:46 -0600

    Updates to custom view

---
+++ InterfaceBuilderTutorial/CustomView.swift
@@ -0,0 +1,21 @@
+//
+//  CustomView.swift
+//  InterfaceBuilderTutorial
+//
+//  Created by Byron Mackay on 5/30/15.
+//  Copyright (c) 2015 me. All rights reserved.
+//
+
+import UIKit
+
+class CustomView: UIButton {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
